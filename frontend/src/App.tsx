fb21378bd7ee4d07839a5ff4eaf8201ed31eaded
leo4410 <l02.seminatore@gmail.com>
2024-12-28 15:39:19 +0100

    remove unused files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,22 @@ const SNOWDATA_URL = "http://localhost:8000/";
 
 function App() {
 
+  /*const [data, setData] = useState()
 
+  <// useEffect(() => {
+    // fetchDailyData(2024, 12, 22);
+  // }, []);
+
+  async function fetchDailyData(year: number, month: number, day: number) {
+
+    var query = SNOWDATA_URL + "snow/year/"
+    query = query + year + "?month=" + month + "&day=" + day;
+
+    var responsedata = await fetch(query);
+    var jsondata = await responsedata.json();
+    setData(jsondata)
+    console.log(jsondata)
+  }*/
 
   return (
     <div>
